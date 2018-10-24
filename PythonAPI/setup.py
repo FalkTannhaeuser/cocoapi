14ab12aca06e8d6e6d9833309c94addb840644bc
Falk Tannhäuser <falk.tannhauser@crf.canon.fr>
2018-10-24 16:59:00 +0200

    Enable compilation under Windows, using MSVC

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, Extension
+import sys
 import numpy as np
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
+
+# Extra arguments for MSVC (cl.exe) or for GCC:
+extra_args = [] if sys.platform == 'win32' else ['-Wno-cpp', '-Wno-unused-function', '-std=c99']
 
 ext_modules = [
     Extension(
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=extra_args,
     )
 ]
 
